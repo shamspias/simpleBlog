8204cd598d65ef493ca9584527024a6d5ad9d3b3
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2021-01-31 17:51:44 +0600

    update
    update

--- fontend/src/components/Home.js
+++ fontend/src/components/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const home = () => (
-    <div>
-    Home
-</div>
+    <div className = 'container'>
+        <div className="jumbotron">
+            <h1 className="display-4">Welcome To OP Blog</h1>
+            <p className="lead">We are the Over Power Blog Who postt all OP staff</p>
+            <hr className="my-4" />
+            <p>Click The Button to cheak OP Blog</p>
+            <Link className="btn btn-primary btn-lg" to='/blog' role="button">Check our Blog</Link>
+        </div>
+    </div>
 );
  
 export default home;
